e50c8f3cce954b69bb9a8fe4a4e24f77811005a5
Abhishek Shandilya <abhishek.sandy@gmail.com>
2015-01-18 07:10:24 +0000

    static controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/resources'
+
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
