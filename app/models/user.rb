ffbfa9ec850c81185d5cc3a170bc182b45ccd342
Abhishek Shandilya <abhishek.sandy@gmail.com>
2015-01-18 10:34:45 +0000

    destroyed user scaffold

--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ActiveRecord::Base
-    validates :roll,    length: { is: 8 },
-                        presence: true
-end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
 
   get 'resource' => 'static_pages#resource'
   
-  resources :users
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
