5329f9a33f0580f5c727683762f6a91d33d316f5
Abhishek Shandilya <abhishek.sandy@gmail.com>
2015-01-18 12:36:39 +0000

    user model set

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'turbolinks',   '2.5.3'
 gem 'jbuilder',     '2.2.6'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc',         '0.4.1', group: :doc
+gem 'bootstrap-sass', '3.3.1.0'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,32 +1,28 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  def setup
-    @base_title = "WebOps Club IITM"
-  end
-  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", full_title()
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", full_title('About')
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+    assert_select "title", full_title('Contact')
   end
 
   test "should get resource" do
     get :resource
     assert_response :success
-    assert_select "title", "Resource | #{@base_title}"
+    assert_select "title", full_title('Resource')
   end
 
 end
