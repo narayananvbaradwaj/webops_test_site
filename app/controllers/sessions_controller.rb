c7d96530f4529f1259af1d6b6737f979e874fcc0
Abhishek Shandilya <abhishek.sandy@gmail.com>
2015-01-18 20:22:11 +0000

    improve cookie test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(roll: params[:session][:roll].downcase)
-    if user && user.authenticate(params[:session][:password])
+    @user = User.find_by(roll: params[:session][:roll].downcase)
+    if @user && @user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
-      flash.now[:success] = 'Welcome back #{user.roll}'
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      flash.now[:success] = 'Welcome back #{@user.roll}'
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination'
