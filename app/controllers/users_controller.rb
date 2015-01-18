a17f2c1872b3cf82fffb685a6d9631ebd1b2833e
Abhishek Shandilya <abhishek.sandy@gmail.com>
2015-01-18 18:14:49 +0000

    flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # Handle a successful save.
+      flash[:success] = "Welcome to the WebOps Club!"
       redirect_to @user
     else
       render 'new'
